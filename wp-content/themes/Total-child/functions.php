a718c0e8171fee10c42864a73036f8cce82d6038
Stefan Iancu <a1b2ff@yahoo.com>
2015-12-30 10:57:49 +0200

    Added slider

--- wp-content/themes/Total-child/functions.php
+++ wp-content/themes/Total-child/functions.php
@@ -4,4 +4,14 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+add_filter('show_admin_bar', '__return_false');
+
+/*add_action('set_current_user', 'csstricks_hide_admin_bar');
+function csstricks_hide_admin_bar() {
+    if (!current_user_can('edit_posts')) {
+        show_admin_bar(false);
+    }
+}*/
+
 ?>
