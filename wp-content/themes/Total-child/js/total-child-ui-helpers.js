a0af65432b9fb3f2719cef51a2727541a3539ba7
Stefan Iancu <a1b2ff@yahoo.com>
2015-12-31 17:41:14 +0200

    Added animations

--- wp-content/themes/Total-child/js/total-child-ui-helpers.js
+++ wp-content/themes/Total-child/js/total-child-ui-helpers.js
@@ -10,5 +10,35 @@ jQuery('a.local-scroll').on('click', function(){
 
 function scrollToElement(aid){
     var element = jQuery(aid);
-    jQuery('html,body').animate({scrollTop: element.offset().top},'slow');
+    element.removeClass('bounce-up');
+    jQuery('html,body').animate({scrollTop: element.offset().top-20},'slow');
+    element.addClass('bounce-up');
+}
+
+var $animation_elements = jQuery('.bounce-up');
+var $window = jQuery(window);
+
+$window.on('scroll resize', check_if_in_view);
+
+$window.trigger('scroll');
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    jQuery.each($animation_elements, function() {
+        var $element = jQuery(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
 }
